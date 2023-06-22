42bedb8d29ab3d0441b40ed3f8bebe9eb4847168
unknown <kalyan.raparthi@hotmail.com>
2023-06-22 10:33:35 +0530

    htmls' removed.

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const ex = require("express");
 const app = ex();
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/front/index.html');
-})
-
-app.get('/o', (req, res) => {
-    res.sendFile(__dirname + '/front/o.html');
-})
-
 app.listen(3000);
