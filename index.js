7a5267d6f4cda8fd570ac6e9f486ab3c8a5fd7f1
unknown <kalyan.raparthi@hotmail.com>
2023-06-23 11:40:26 +0530

    post_request.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const mongoose = require("mongoose"); 
 const ex = require("express");
 const app = ex();
+ 
+app.post('/', (req, res) => {
+    console.log(req);
+    res.send("[v]POST-REQUEST");
+});
 
 app.listen(3000);
